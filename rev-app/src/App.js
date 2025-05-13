4f96c960765506073df0ad0f4aa019877572eb99
shahil <shahil.s9934@gmail.com>
2025-05-13 20:35:32 +0530

    add new commit

--- rev-app/src/App.js
+++ rev-app/src/App.js
@@ -1,8 +1,53 @@
+import './App.css';
+
 const App = () => {
   return(
-    <>
-    <h5>App component</h5>
-    </>
+   <div className = "container">
+
+    <div className="item1">
+      <h4>Unlocking the Power of Digital Curiosity 1</h4>
+      <p>
+      In today's fast-paced tech landscape, curiosity 
+      is more than a trait—it's a competitive advantage. 
+      Teams that foster a culture of digital exploration 
+      often discover smarter workflows, faster tools, and 
+      creative solutions before their competitors. Whether 
+      it's experimenting with microservices, diving into
+       AI-driven insights, or rethinking legacy architectures,
+        staying curious fuels continuous innovation.
+      </p>
+    </div>
+
+    <div className="item2">
+      <h4>Unlocking the Power of Digital Curiosity 2</h4>
+      <p>
+      In today's fast-paced tech landscape, curiosity 
+      is more than a trait—it's a competitive advantage. 
+      Teams that foster a culture of digital exploration 
+      often discover smarter workflows, faster tools, and 
+      creative solutions before their competitors. Whether 
+      it's experimenting with microservices, diving into
+       AI-driven insights, or rethinking legacy architectures,
+        staying curious fuels continuous innovation.
+      </p>
+    </div>
+    <div className="item3">
+      <h4>Unlocking the Power of Digital Curiosity 3</h4>
+      <p>
+      In today's fast-paced tech landscape, curiosity 
+      is more than a trait—it's a competitive advantage. 
+      Teams that foster a culture of digital exploration 
+      often discover smarter workflows, faster tools, and 
+      creative solutions before their competitors. Whether 
+      it's experimenting with microservices, diving into
+       AI-driven insights, or rethinking legacy architectures,
+        staying curious fuels continuous innovation.
+      </p>
+    </div>
+
+
+   </div>
+   
   )
 }
 
